40e7dc6e28cb82cb6bb297015d459bdfffe748db
RandomPhrases <60932177+RandomPhrases@users.noreply.github.com>
2020-02-11 16:24:51 +0100

    delete

--- azar.py
+++
@@ -1,9 +0,0 @@
-
-
-import random
-
-frases = ["Frase 1","Frase 2","Frase 3","Frase 4","Frase 5","Frase 6","Frase 7","Frase 8","Frase 9","Frase 10"]
-
-numero = random.randint(0,10)
-
-return frases[numero]
